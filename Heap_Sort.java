8087c8199562a685154a932a39e2f3b31b4fd00b
Jaydip Makwana <73463542+jaydip9548@users.noreply.github.com>
2021-08-28 05:52:53 +0530

    Update Heap_Sort.java

--- Heap_Sort.java
+++ Heap_Sort.java
@@ -1,62 +1,47 @@
 import java.util.*;
-public class Heap_Sort {
+import java.text.DecimalFormat;
+public class Main {
 	
-	public static void Heap(int a[], int n){
-		int temp;
-		int b[] = new int[n];
-		for(int i=n/2 - 1; i>=0;i--) {
-			Heap1(a,i,n);
-		}
+	public static void main(String [] args){
 	
-		for(int i=n-1; i>=0;i--) {
-			b[i] = a[0];
-			a[0] = a[i];
-			a[i]=0;
-			Heap1(a,0,i);
-		}
-		
-		System.out.println("Sorted Numbers are ");
-		for(int i : b) {
-		System.out.println(i);
+	int arr[] = {20,50,30,75,90,60,25,10,40};
+	int n = arr.length;
+	
+	for(int i = n/2 - 1; i>=0; i--){
+	    maxHeapify(arr,n,i);
+	}
+		System.out.println(Arrays.toString(arr));
+	
+	for(int i=n-1; i>=0; i--){
+	    swap(arr,i,0);
+	    maxHeapify(arr,i,0);
 	}
 	
-	}
-	public static void Heap1(int a[],int j,int n)
-	{  int temp=0;
-		int largest = j;
-		int left = 2*j + 1;
-		int right = 2*j + 2;
-		
-		if(left < n && a[left] >a[largest]) {
-			largest = left;
-		}
-		if(right < n && a[right] > a[largest]) {
-			largest = right;
-		}
-		
-		if(largest != j) {
-			temp = a[j];
-			a[j] = a[largest];
-			a[largest] = temp;
-			
-			Heap1(a,largest,n);
-		}
-	}
-public static void main(String[] args) {
-long start = System.currentTimeMillis();
-	Scanner sc = new Scanner(System.in);
-	System.out.println("Enter Number You Want to Insert:");
-	int n = sc.nextInt();
-	int a[] = new int [n];
-	for(int i=0; i<n;i++) {
-		a[i] = sc.nextInt();
-	}
-	
-	Heap(a,n);
-	long end = System.currentTimeMillis();
-	
-	System.out.println("Total Time : "+(end-start)/1000 +" Second");
-	
-	
+
+}
+
+public static void maxHeapify(int arr[],int n,int i){
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2*i + 2;
+    
+    while(left < n && arr[left] > arr[largest]){
+        largest = left;
+    }
+    
+    while(right < n && arr[right] >  arr[largest]){
+        largest = right;
+    }
+    if(largest != i){
+        swap(arr,largest,i);
+        maxHeapify(arr,n,largest);
+    }
+}
+
+public static void swap(int arr[],int l,int i){
+    int temp  = arr[l];
+    arr[l] = arr[i];
+    arr[i] = temp;
 }
 }
+
