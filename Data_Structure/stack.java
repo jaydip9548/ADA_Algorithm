9d54d415672f3741dfae0d574c2c1bdb9735a106
jaydip9548 <2018itjay066@ldce.ac.in>
2021-09-21 08:14:44 +0530

    stack implementation using Array

--- Data_Structure/stack.java
+++ Data_Structure/stack.java
@@ -1,4 +1,57 @@
 package Data_Structure;
 
+
 public class stack {
+   static int arr[] = new int[100];
+   static int top = -1;
+   static int max = 100;
+
+    public static void main(String[] args) {
+
+        push(10);push(20);push(30);push(40);
+        pop();
+        peek();
+        display();
+    }
+
+    private static void display() {
+        while(top != -1){
+            System.out.println(arr[top--]);
+        }
+    }
+
+    private static void peek() {
+        if(top == -1){
+            System.out.println("Stack is underflow");
+            return;
+        }else{
+            System.out.println("Peek value is : "+arr[top]);
+        }
+    }
+
+    private static void pop() {
+
+        if(top == -1){
+            System.out.println("Stack is underflow");
+            return;
+        }else{
+            System.out.println(arr[top] +" is removed");
+            arr[top] = 0;
+            top--;
+        }
+    }
+
+    private static void push(int i) {
+        if(top == max-1 )
+        {
+            System.out.println("Stack is Overflow ");
+            return;
+        }
+        else{
+            top++;
+            arr[top ] = i;
+        }
+
+    }
+
 }
