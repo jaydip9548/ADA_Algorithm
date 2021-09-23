546d5e6eb32cce1e35b275ac8149eb996060d730
jaydip9548 <2018itjay066@ldce.ac.in>
2021-09-23 06:06:40 +0530

    BFS

--- Graph/Adjacency_List.java
+++ Graph/Adjacency_List.java
@@ -1,6 +1,7 @@
 package Graph;
 
 import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 //https://youtu.be/nt8uN6pQrnw
@@ -24,6 +25,41 @@ public class Adjacency_List {
         adj[destination].add(source);
     }
 
+    public static int bfs(int source, int destination) {
+        boolean vis[] = new boolean[adj.length];
+        int parent[] = new int[adj.length];
+        Queue<Integer> q = new LinkedList<>();
+
+        q.add(source);
+        parent[source] = -1;
+        vis[source] = true;
+
+//        System.out.println(source);
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            if (cur == destination) break;
+            for (int neightbor : adj[cur]) {
+
+                if (!vis[neightbor]) {
+//                    System.out.println(neightbor);
+                    vis[neightbor] = true;
+                    q.add(neightbor);
+                    parent[neightbor] = cur;
+                }
+            }
+        }
+
+        int cur = destination;
+        int distance = 0;
+        while (parent[cur] != -1) {
+            System.out.print(cur + " -> ");
+            cur = parent[cur];
+            distance++;
+        }
+        System.out.print(source );
+        return distance;
+    }
+
     public static void main(String[] args) {
 
         int v = 5;
@@ -31,15 +67,15 @@ public class Adjacency_List {
 
         Adjacency_List list = new Adjacency_List(v);
 
+        list.addEdge(0, 1);
         list.addEdge(1, 2);
-        list.addEdge(1, 4);
         list.addEdge(2, 3);
         list.addEdge(3, 4);
         list.addEdge(0, 4);
-        list.addEdge(0, 3);
+        list.addEdge(2, 4);
 
-        display();
-
+//        display();
+        bfs(0, 4);
     }
 
     static void display() {
